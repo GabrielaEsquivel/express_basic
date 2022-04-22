1400b61d1c98cc060eb82dcea46f1522f856393e
Andrea <andrea.rueda-ext@dominion-global.com>
2022-04-21 20:35:36 -0500

    add another route

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
     res.send("Hola a todos los explorers! los admiro mucho!")
 });
 
+app.get('/bienvenida', (req, res) => {
+    res.send("Bienvenidos a todos! Este es mi primer server")
+});
+
 app.listen(port, () => {
     console.log("server escuchando")
 } )
